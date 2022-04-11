e98970b907ca5f1d8931234b48d70d5e19e2fb9d
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-11 22:39:19 +0000

    added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,22 @@
+const sentence = "hello there from lighthouse labs";
+// const timeout = function(char, time) {
+//   setTimeout(() => {
+//     process.stdout.write(char);
+//   }, time)
+// };
+
+// let time = 0;
+// for (let char of sentence) {
+//   time += 50;
+//   timeout(char, time);
+// }
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+  }, i * 50);
+}
+
+setTimeout(() => {
+  console.log();
+}, sentence.length * 50);
